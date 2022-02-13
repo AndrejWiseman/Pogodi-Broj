3f912d1065a62e70243c82dc22e03197e48c92ec
A.M <andrej2000aa@gmail.com>
2022-02-13 11:22:46 +0100

    ispravka prve bmi

--- bmiIndex/script.js
+++ bmiIndex/script.js
@@ -8,6 +8,20 @@ let izrazito = document.querySelector('.izrazito').innerHTML;
 let stisak1 = document.querySelector('.btn1');
 
 
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+
 //stisak1.addEventListener('click', function(){
 //
 //    let visina = document.getElementById('visina').value;
